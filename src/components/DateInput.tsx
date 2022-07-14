76af3a3d4de57adfc8003df652f4f4b55ec4992b
Dmytro Zemnytskyi <pragmasoft@gmail.com>
2022-07-14 14:26:58 +0300

    added DateInput

---
+++ src/components/DateInput.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { assert } from "../utils";
+
+const htmlDateFormat = (date: Date) => date.toISOString().split("T")[0];
+
+type HTMLInputProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
+
+type DateInputProps = Omit<HTMLInputProps, "type" | "inputMode">;
+
+const EXPECTED_REF_MSG = "ref.current is expected";
+
+export const DateInput = React.forwardRef<HTMLInputElement, DateInputProps>(
+  (props, forwardedRef) => {
+    const ref = React.useRef<HTMLInputElement>(null);
+
+    const proxyHandler: ProxyHandler<HTMLInputElement> = React.useMemo(
+      () => ({
+        get(target, p) {
+          return target[p as keyof HTMLInputElement];
+        },
+        set(target, p, val) {
+          if (p === "value" && val instanceof Date) {
+            target.value = htmlDateFormat(val);
+          } else {
+            (target as Record<typeof p, typeof val>)[p] = val;
+          }
+          return true;
+        },
+      }),
+      []
+    );
+
+    React.useImperativeHandle(
+      forwardedRef,
+      () => {
+        assert(!!ref.current, EXPECTED_REF_MSG);
+        return new Proxy(ref.current, proxyHandler);
+      },
+      [proxyHandler]
+    );
+
+    return <input type="date" {...props} ref={ref} />;
+  }
+);
+DateInput.displayName = "DateInput";
